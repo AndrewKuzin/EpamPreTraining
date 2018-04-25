1cbaac824f4780aaa4ef77dd4246f068ce56476e
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-04-25 20:11:09 +0300

    Create Main.java

---
+++ Task1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        Tester tester = new Tester();
+        tester.testInt();
+        tester.testByte();
+        tester.testShort();
+        tester.testLong();
+        tester.testDouble();
+        tester.testFloat();
+        tester.testChar();
+        tester.testBoolean();
+    }
+}
