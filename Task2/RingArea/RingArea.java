53a1e92d882a5ca7dd8b6fbb9552d0ba7af9f329
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-01 17:52:54 +0300

    Update RingArea.java

--- Task2/RingArea/RingArea.java
+++ Task2/RingArea/RingArea.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        RingArea.getArea();
+import java.util.Scanner;
+
+public class RingArea {
+    public static void getArea() {
+        System.out.println("input R1:");
+        Scanner in1 = new Scanner(System.in);
+        int r1 = in1.nextInt();
+        System.out.println("input R2:");
+        Scanner in2 = new Scanner(System.in);
+        int r2 = in2.nextInt();
+        System.out.printf("ring area = %.3f", countArea(r1, r2));
+    }
+
+    private static double countArea(int r1, int r2) {
+        double area1 = Math.PI * Math.pow(r1, 2);
+        double area2 = Math.PI * Math.pow(r2, 2);
+        return area1 - area2;
     }
 }
