da2e09040a3ac0c2b09ab272ef5addcd6436436a
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-03 20:45:08 +0300

    Add files via upload

---
+++ Task3/MoodSensor/MoodSensor.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+/**
+ * Created by АВК on 03.05.2018.
+ */
+public class MoodSensor {
+    public static final int MOOD_NUMBER = 3;
+    public static final String SMILE = ":)";
+    public static final String SAD = ":(";
+    public static final String POKER = ":|";
+
+    public static String showMood() {
+        Random random = new Random();
+        String mood = POKER;
+        int a = random.nextInt(MOOD_NUMBER);
+
+        switch (a) {
+            case 0:
+                mood = SMILE;
+                break;
+            case 1:
+                mood = SAD;
+                break;
+            case 2:
+                mood = POKER;
+                break;
+        }
+        return mood;
+    }
+}
