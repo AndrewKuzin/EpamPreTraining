f4327058062cefb114425b1a2fb6985470306512
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-03 20:42:08 +0300

    Create Main.java

---
+++ Task3/Triangle/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a[] = new int[]{1, 2};
+        int b[] = new int[]{2, 2};
+        int c[] = new int[]{3, 1};
+        System.out.println(Triangle.checkIfTriangle(a, b, c));
+    }
+}
