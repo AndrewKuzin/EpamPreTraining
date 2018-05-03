5b15217d466b39338c424b8d47e303cd52637ccc
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-03 20:48:13 +0300

    Create Main.java

---
+++ Task3/CheckLetter/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println(LetterChecker.checkLetter1('A'));
+        System.out.println(LetterChecker.checkLetter2('i'));
+    }
+}
