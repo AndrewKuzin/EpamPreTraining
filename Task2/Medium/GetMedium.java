10281c2ddfe24789037f99c930f793cc8fd59465
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-05 17:48:12 +0300

    Update GetMedium.java

--- Task2/Medium/GetMedium.java
+++ Task2/Medium/GetMedium.java
@@ -1,28 +1,39 @@
-import java.util.Scanner;
-
 public class GetMedium {
-    public static void getMedium() {
-        System.out.println("input number:");
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
+    public static void getMedium(int number) {
         int numbers[] = getNumbers(number);
         double sum = 0, multiply = 1;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
-            multiply *= numbers[i];
-        }
+        sum += numbers[0];
+        multiply *= numbers[0];
+        sum += numbers[1];
+        multiply *= numbers[1];
+        sum += numbers[2];
+        multiply *= numbers[2];
+        sum += numbers[3];
+        multiply *= numbers[3];
+        sum += numbers[4];
+        multiply *= numbers[4];
+        sum += numbers[5];
+        multiply *= numbers[5];
+
         System.out.println("sum = " + sum / numbers.length);
         double algMedium = Math.pow(multiply, 1.0 / numbers.length);
         System.out.printf("multiply = %.3f", algMedium);
     }
 
     private static int[] getNumbers(int number) {
-        int i = 0, numbers[] = new int[6];
-        while (number > 0) {
-            numbers[i] = number % 10;
-            number /= 10;
-            i++;
-        }
+        int numbers[] = new int[6];
+        numbers[0] = number % 10;
+        number /= 10;
+        numbers[1] = number % 10;
+        number /= 10;
+        numbers[2] = number % 10;
+        number /= 10;
+        numbers[3] = number % 10;
+        number /= 10;
+        numbers[4] = number % 10;
+        number /= 10;
+        numbers[5] = number % 10;
+
         return numbers;
     }
 }
