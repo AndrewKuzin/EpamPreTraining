2c72900f3a72350372d4b794cbbf939ecb150a81
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-05 17:56:00 +0300

    Update DinoMass.java

--- Task2/DinoMass/DinoMass.java
+++ Task2/DinoMass/DinoMass.java
@@ -4,10 +4,11 @@ public class DinoMass {
     public static final int GRAMS_IN_KILO = 1000;
     public static final float KILOS_IN_TON = 1000F;
 
-    public static void countMass(int mass) {
-        System.out.println("mass in kilos = " + mass);
-        System.out.println("mass in milligrams = " + mass * MILLIS_IN_KILO);
-        System.out.println("mass in grams = " + mass * GRAMS_IN_KILO);
-        System.out.println("mass in tons = " + mass / KILOS_IN_TON);
+    public static float[] countMass(int mass) {
+        float[] differentMass = new float[3];
+        differentMass[0] = mass * MILLIS_IN_KILO;
+        differentMass[1] = mass * GRAMS_IN_KILO;
+        differentMass[2] = mass / KILOS_IN_TON;
+        return differentMass;
     }
 }
