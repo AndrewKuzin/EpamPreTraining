2b43e778882038bc39971bb9b550a57bbabcce74
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-05 18:29:55 +0300

    Update Main.java

--- Task2/SumAndMultiply/Main.java
+++ Task2/SumAndMultiply/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        SumAndMultiply.getSumAndMult();
+        int[] sumAndMult = SumAndMultiply.getSumAndMult(12345);
+        System.out.println("sum = " + sumAndMult[0]);
+        System.out.println("mult = " + sumAndMult[1]);
     }
 }
