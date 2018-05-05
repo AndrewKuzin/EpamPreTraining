800477fe914e1831029f9ff455c52d7217ba1a1c
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-05 18:08:17 +0300

    Update Main.java

--- Task2/Medium/Main.java
+++ Task2/Medium/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        GetMedium.getMedium(123456);
+        double[] medium = GetMedium.getMedium(123456);
+        System.out.println("algebraic = "+medium[0]);
+        System.out.println("arithmetic = "+medium[1]);
     }
 }
