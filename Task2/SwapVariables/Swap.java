4b1d441b1862efcafedb9b1e1c5daecec5d3dae9
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-05 18:36:49 +0300

    Update Swap.java

--- Task2/SwapVariables/Swap.java
+++ Task2/SwapVariables/Swap.java
@@ -1,17 +1,9 @@
-import java.util.Scanner;
-
 public class Swap {
-    public static void getSwap() {
-        System.out.println("input a:");
-        Scanner in1 = new Scanner(System.in);
-        int a = in1.nextInt();
-        System.out.println("input b:");
-        Scanner in2 = new Scanner(System.in);
-        int b = in2.nextInt();
-        a += b;
-        b = a - b;
-        a -= b;
-        System.out.println("swaped a: " + a);
-        System.out.println("swaped b: " + b);
+    public static int[] getSwap(int a, int b) {
+        a ^= b;
+        b = a ^ b;
+        a ^= b;
+        int[] swaped = {a, b};
+        return (swaped);
     }
 }
