247ca11ca8db6ec1ac2c33d2ed9573f547bd5bbc
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-05 17:56:38 +0300

    Update Main.java

--- Task2/DinoMass/Main.java
+++ Task2/DinoMass/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        DinoMass.countMass(1234);
+        float[] mass = DinoMass.countMass(1234);
+        System.out.println("mass in milligrams = " + mass[0]);
+        System.out.println("mass in grams = " + mass[1]);
+        System.out.println("mass in tons = " + mass[2]);
     }
 }
