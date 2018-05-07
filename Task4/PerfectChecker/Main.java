13b7d1a648cffe680144192b3a655ad2e1a2aa89
AndrewKuzin <38350376+AndrewKuzin@users.noreply.github.com>
2018-05-07 15:26:38 +0300

    Create Main.java

---
+++ Task4/PerfectChecker/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PerfectChecker.checkPerfect(6));
+        System.out.println(PerfectChecker.checkPerfect(28));
+        System.out.println(PerfectChecker.checkPerfect(496));
+        System.out.println(PerfectChecker.checkPerfect(8128));
+    }
+}
